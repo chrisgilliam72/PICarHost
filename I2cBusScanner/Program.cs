4a1a0e9abe283e65d205c552f7ab7a21d56da935
chrisgilliam72 <54978118+chrisgilliam72@users.noreply.github.com>
2022-07-24 16:01:10 +0200

    Add files via upload

---
+++ I2cBusScanner/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Iot.Device.Board;
+using System.Device.I2c;
+
+Console.WriteLine("Bus Scanner started");
+var raspberryPibrd = new RaspberryPiBoard();
+var controller = raspberryPibrd.CreateGpioController();
+
+int busNo = raspberryPibrd.GetDefaultI2cBusNumber();
+Console.WriteLine("Bus No:" + busNo);
+var ic2Bus = I2cBus.Create(busNo);
+var busItems = I2cBusExtensions.PerformBusScan(ic2Bus);
+
+Console.WriteLine("Found Items:" + busNo);
+foreach (var busItem in busItems)
+{
+    Console.WriteLine(busItem.ToString());
+
+}
+Console.WriteLine("Scanning for ultraborg on Bus:" + busNo);
+foreach (var busItem in busItems)
+{
+    byte[] recBuffer = new byte[4];
+    var deviceItem = ic2Bus.CreateDevice(busItem);
+    try
+    {
+        Console.WriteLine($"Writting to device: {busItem}");
+        deviceItem.WriteByte(0x99);
+        deviceItem.Read(recBuffer);
+        Console.WriteLine($"Adress {busItem}: result : {recBuffer[1]}");
+        if (recBuffer[1]==0x36)
+            Console.WriteLine($"Ultraborg found on address: {busItem}");
+    }
+    catch
+    {
+        Console.WriteLine($"result : failed");
+    }
+
+}
+Console.ReadLine();
