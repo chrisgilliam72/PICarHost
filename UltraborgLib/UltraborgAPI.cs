9b5741a348be7402a6b9a01298cf91e5ec07ab0e
Chris Gilliam <chrisgilliam1972@gmail.com>
2024-01-05 20:30:58 +0200

    Added UltraborgAPI interface and upgraded nuget packages

---
+++ UltraborgLib/UltraborgAPI.cs
@@ -0,0 +1,48 @@
+namespace Ultraborg
+{
+    using Iot.Device.Board;
+    using Microsoft.Extensions.Logging;
+
+    public interface IUltraborgAPI
+    {
+        bool Setup();
+        double GetDistance(int sensorNo);
+    }
+
+    public class UltraborgAPI : IUltraborgAPI
+    {
+
+        private readonly Ultraborg.Library.Ultraborg ultraborg;
+        private readonly ILogger logger;
+        public UltraborgAPI(ILoggerFactory loggerFactory)
+        {
+            logger=loggerFactory.CreateLogger("Ultraborg");
+            ultraborg=new Ultraborg.Library.Ultraborg();
+        }
+
+        public bool Setup()
+        {
+            var raspberryPibrd = new RaspberryPiBoard();
+            var controller = raspberryPibrd.CreateGpioController();
+
+            int busNo = raspberryPibrd.GetDefaultI2cBusNumber();
+            logger.LogInformation("Bus No:" + busNo);
+
+            int ultraborgAddress = ultraborg.GetUltraBorgAdress();
+            if (ultraborgAddress>0)
+            {
+                logger.LogInformation("Address: "+ultraborgAddress);
+                ultraborg.Init(busNo,ultraborgAddress);
+                return true;
+            }
+
+            logger.LogCritical("Ultraborg not detected");
+            return false;
+        }
+
+        public double GetDistance(int sensorNo)
+        {
+            return ultraborg.GetDistance(sensorNo);
+        }
+    }
+}
--- UltraborgWebAPI/Program.cs
+++ UltraborgWebAPI/Program.cs
@@ -1,14 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Mvc;
+using Ultraborg;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IUltraborgAPI,UltraborgAPI>();
 
 var app = builder.Build();
-var ultraBorg = new Ultraborg.Library.Ultraborg();
-int address = ultraBorg.GetUltraBorgAdress();
-ultraBorg.Init(1, address);
+var ultraBorgAPI=app.Services.GetService<IUltraborgAPI>();
+ultraBorgAPI.Setup();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,20 +24,20 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.MapGet("/Distance/{sensorNo}", (int sensorNo) =>
+app.MapGet("/Distance/{sensorNo}", ([FromServices]IUltraborgAPI ultraBorg, int sensorNo) =>
 {
 
-    double distance = sensorNo switch
-    {
-        1 => ultraBorg.GetFilteredDistance1(),
-        2 => ultraBorg.GetFilteredDistance2(),
-        3 => ultraBorg.GetFilteredDistance3(),
-        4 => ultraBorg.GetFilteredDistance4(),
-        _ => -1
+    // double distance = sensorNo switch
+    // {
+    //     1 => ultraBorg.GetFilteredDistance1(),
+    //     2 => ultraBorg.GetFilteredDistance2(),
+    //     3 => ultraBorg.GetFilteredDistance3(),
+    //     4 => ultraBorg.GetFilteredDistance4(),
+    //     _ => -1
 
-    };
+    // };
 
-    return distance;
+    return ultraBorg.GetDistance(sensorNo);
 }).WithName("GetDistance").WithOpenApi();
 app.Run("http://localhost:8090");
 
